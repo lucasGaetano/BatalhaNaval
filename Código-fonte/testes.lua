b78641da5a4a935158fa4c1f1e95758ca00bba30
Lucas Augusto <luccasgaetano19@gmail.com>
2016-06-17 00:25:23 -0300

    teste 1 para a adaptação do número de posição de cada navio

--- Código-fonte/testes.lua
+++ Código-fonte/testes.lua
@@ -1,1 +1,57 @@
-luaunit = require('luaunit')function verificaBarcos (tabuleiro)	local contador = 0	for linha=1,10 do		for coluna=1,10 do			if(tabuleiro[linha][coluna] == 1) then				contador = contador + 1			end		end		return contador	endendTestesTabuleiro{} --classe de testes	function TestesTabuleiro:verificaSeTabuleiroTemNavios()		luaunit.assertEquals(verificaBarcos(t),3)os.exit( luaunit.LuaUnit.run())
+listaNavios = {}
+listaNavios[1] = {}
+listaNavios[2] = {}
+listaNavios[3] = {}
+
+--~ for i = 1, 4 do
+--~ 	if(navios[2][i] ~= nil) then
+--~ 		print(navios[2][i])
+--~ 	else
+--~ 		i = 4
+--~ 	end
+--~ end
+rodada = 2
+
+if (rodada == 1) then
+	print("Posicione o "..rodada.."° navio: ")
+	print("X:") x = io.read("*number")
+	print("Y:") y = io.read("*number")
+
+	listaNavios[rodada] = {x, y}
+
+elseif (rodada == 2) then
+	print("Posição inicial do "..rodada.."° navio: ")
+	print("X:") x = io.read("*number")
+	print("Y:") y = io.read("*number")
+
+	listaNavios[rodada] = {x, y}
+
+	print("Posição final do "..rodada.."° navio: ")
+	print("X:") x = io.read("*number")
+	print("Y:") y = io.read("*number")
+
+	table.insert(listaNavios[rodada], x, y)
+
+elseif (rodada == 3) then
+	print("Posição inicial do "..rodada.."° navio: ")
+	print("X:") x = io.read("*number")
+	print("Y:") y = io.read("*number")
+
+	listaNavios[rodada] = {x, y}
+
+	print("Posição final do "..rodada.."° navio: ")
+	print("X:") x = io.read("*number")
+	print("Y:") y = io.read("*number")
+
+	if(listaNavios[3][1] == x)
+
+	table.insert(listaNavios[rodada], x, y)
+end
+
+for i = 1, 10 do
+	if(listaNavios[2][i] ~= nil) then
+		print(listaNavios[2][i])
+	else
+		i = 10
+	end
+end
